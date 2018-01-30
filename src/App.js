09173975123968648f10b26640892a1aea65dcc3
jabram <jaime@cantina.co>
2018-01-30 18:59:35 -0500

    Redesigned everything

--- src/App.js
+++ src/App.js
@@ -1,129 +1,175 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MOCKS from './mock.json';
 
-
-function Header(props) {
+function Payment(props) {
   return (
-    <div className="header">
-      <a href="/dashboard">dashboard</a>
-      <div className="actions">
-        <a href="/people">People</a>
-        <a href="/settings">Settings</a>
-      </div>
-      <h1>The Name of This Divvy</h1>
+    <div className="payment grid-row">
+      <span className="date">{props.payment.date}</span>
+      <span className="person">{props.payment.person}</span>
+      <span className="description">{props.payment.description}</span>
+      <span className="amount">{props.payment.amount}</span>
+      <span className="paid">{props.payment.paid}</span>
+      <span className="remaining">{props.payment.left}</span>
     </div>
   )
 }
 
-function TotalBalance(props) {
-  return (
-    <div className="totals">
-      <h2>I owe/I am owed <span className="currency">$1000.00</span></h2>
+function Payments(props) {
+  const noPayments = (
+    <p className="no-content">nuttin yet</p>
+  );
 
-      <div className="breakdown-by-person">
-        <h3>breakdown:</h3>
-        <div>
-          <p className="key">red</p>
-          <p className="name">Person A</p>
-          <p className="owes">owes me</p>
-          <p className="currency">$100.00</p>
-        </div>
-        <div>
-          <p className="key">orange</p>
-          <p className="name">Person B</p>
-          <p className="owes">I owe</p>
-          <p className="currency">$10,000.00</p>
-        </div>
-        <div>
-          <p className="key">yellow</p>
-          <p className="name">Person C</p>
-          <p className="owes">even</p>
-          <p className="currency">$0.00</p>
-        </div>
-        <div>
-          <p className="key">green</p>
-          <p className="name">Person D</p>
-          <p className="owes">owes me</p>
-          <p className="currency">$20.00</p>
-        </div>
+  let payments = [];
+  if (props.payments) {
+    props.payments.map((payment) => {
+      return payments.push(<Payment payment={payment} />);
+    });
+  }
+
+  return (
+    <div className="payments">
+      <h3 className="header">Payback History</h3>
+      <div className="payment grid-row header">
+        <span className="date">Date</span>
+        <span className="person">Person</span>
+        <span className="description">Description</span>
+        <span className="amount">Amount</span>
+        <span className="paid">Total Paid</span>
+        <span className="remaining">Now Left</span>
       </div>
+      {payments.length ? payments : noPayments}
+      <button className="add-payment">+ add payback</button>
     </div>
   )
 }
 
-function IndividualResponsibility(props) {
+function Person(props) {
   return (
-    <div>
-      <p className="person">{props.individual.person}</p>
-      <p className="pay">{props.individual.pay}</p>
-      <p className="owe">{props.individual.owe}</p>
+    <div className="person grid-row">
+      <span className="name">{props.person.name}</span>
+      <span className="breakdown">
+        $<input type="text" className="amount" />
+        <input type="text" className="percentage" />%
+      </span>
+      <span className="should">{props.person.should}</span>
+      <span className="paid">{props.person.paid}</span>
+      <span className="remaining">{props.person.remaining}</span>
     </div>
-  );
+  )
 }
 
-function TransactionBreakdown(props) {
-  const fullBreakdown = [];
-  props.breakdown.forEach((individual, index) => {
-    fullBreakdown.push(
-      <IndividualResponsibility individual={individual} key={index} />
-    );
-  });
+function People(props) {
+  const noPeople = (
+    <p className="no-content">oh well ain't no people here</p>
+  );
+
+  let people = [];
+  if (props.people) {
+    props.people.map((person) => {
+      return people.push(<Person key={person.id} person={person} />);
+    });
+  }
 
   return (
-    <div className="breakdown-by-person">
-      <p className="breakdown">breakdown:</p>
-      <div className="header">
-        <p className="person">person</p>
-        <p className="pay">will pay</p>
-        <p className="owe">will owe</p>
+    <div className="people">
+      <h3 className="header">Breakdown by Person</h3>
+      <div className="person grid-row header">
+        <span className="name">Person</span>
+        <span className="breakdown">Breakdown</span>
+        <span className="should">Should</span>
+        <span className="paid">Paid</span>
+        <span className="remaining">Left</span>
       </div>
-      {fullBreakdown}
+      {people.length ? people : noPeople}
+      <button className="add-person">+ add person</button>
     </div>
-  );
+  )
 }
 
-function Transaction(props) {
+function TransactionDetails(props) {
   return (
-    <div className="transaction">
-      <p className="date">{props.transaction.date}</p>
-      <p className="description">{props.transaction.description}</p>
-      <p className="amount currency">{props.transaction.amount}</p>
-      <p className="my-delta currency">{props.transaction.myDelta}</p>
-      <p className="toggle"><button>+ expand</button></p>
-      <TransactionBreakdown breakdown={props.transaction.breakdown} />
+    <div className="transaction-details">
+      <People people={props.people} />
+      <Payments payments={props.payments} />
     </div>
-  );
+  )
+}
+
+class Transaction extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      expanded: !this.state.expanded
+    })
+  }
+
+  render() {
+    let transactionClassNames = ['transaction'];
+    if (this.state.expanded === true) {
+      transactionClassNames.push('expanded');
+    }
+
+    return (
+      <div className={transactionClassNames.join(' ')}>
+        <div className="transaction-main grid-row">
+          <span className="date">{this.props.transaction.date}</span>
+          <span className="description left">{this.props.transaction.description}</span>
+          <span className="amount">{this.props.transaction.amount}</span>
+          <span className="my-stats">paid/should</span>
+          <span className="remaining">left goes here</span>
+          <span className="toggle-details" onClick={() => this.handleClick()}>
+            <button>
+              {this.state.expanded === true ? '-' : '+'}
+            </button>
+          </span>
+        </div>
+        <TransactionDetails
+          people={this.props.transaction.people}
+          payments={this.props.transaction.payments} />
+      </div>
+    )
+  }
 }
 
 function Transactions(props) {
-  const allTransactions = [];
-  props.transactions.forEach((transaction, index) => {
-    allTransactions.push(
-      <Transaction transaction={transaction} key={index} />
-    )
+  const noTransactions = (
+    <p className="no-content">no transactions yet!</p>
+  );
+
+  const transactions = [];
+  props.transactions.map((transaction) => {
+    return transactions.push(<Transaction key={transaction.id} transaction={transaction} />);
   });
+
   return (
     <div className="transactions">
-      <h2>All Transactions</h2>
-      <div className="header">
-        <p className="date">date</p>
-        <p className="description">description</p>
-        <p className="amount">amount</p>
-        <p className="my-delta">my &Delta;</p>
-        <p className="toggle"></p>
+      <h2>this is the transactions duh</h2>
+      <div className="transaction-main grid-row header">
+        <span className="date">date</span>
+        <span className="description left">description</span>
+        <span className="amount">amount</span>
+        <span className="my-stats">paid/should</span>
+        <span className="remaining">remaining</span>
+        <span className="toggle-details"></span>
       </div>
-      {allTransactions}
+        { transactions.length ? transactions : noTransactions }
     </div>
-  );
+  )
 }
 
-
 export default function App(props) {
+
   return (
     <div>
-      <Header />
-      <TotalBalance />
+      <h1>it's the app!</h1>
       <Transactions transactions={MOCKS} />
+      <p>TODO: add transaction button goes here</p>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
+import './css/styles.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
